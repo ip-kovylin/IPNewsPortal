3f791c7e635767c7eb17c2f1af5d14275619dbff
ip.kovylin <ip.kovylin@yandex.ru>
2023-04-04 15:06:47 +0300

    Installed Bootstrap template

--- IPNP/IPNP/settings.py
+++ IPNP/IPNP/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+
     'PortalApp'
 ]
 
@@ -125,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
