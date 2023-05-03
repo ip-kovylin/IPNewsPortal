198940a7c7702e29419c740b5a86f4169f92dc9f
ip.kovylin <ip.kovylin@yandex.ru>
2023-05-03 13:14:05 +0300

    Limit of 3 posts per day added

--- IPNP/PortalApp/models.py
+++ IPNP/PortalApp/models.py
@@ -22,7 +22,7 @@ class Author(models.Model):
         self.save()
 
     def __str__(self):
-        return f'{self.user}, rating: {self.rating}'
+        return f'{self.user}'
 
 
 class Category(models.Model):
